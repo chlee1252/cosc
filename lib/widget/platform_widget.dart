e296ea9ea59b317917f13b32471c932e6afae41b
Marc <chlee1252@gmail.com>
2022-12-27 21:17:16 +0900

    fix: early return으로 변경

--- lib/widget/platform_widget.dart
+++ lib/widget/platform_widget.dart
@@ -6,13 +6,14 @@ abstract class PlatformWidget extends StatelessWidget {
   const PlatformWidget({Key? key}) : super(key: key);
 
   Widget buildCupertinoWidget(BuildContext context);
+
   Widget buildMaterialWidget(BuildContext context);
+
   @override
   Widget build(BuildContext context) {
     if (Platform.isIOS) {
       return buildCupertinoWidget(context);
-    } else {
-      return buildMaterialWidget(context);
     }
+    return buildMaterialWidget(context);
   }
 }
