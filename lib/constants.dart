805093a742f468d29e5ca6d76780abe097cb9ab9
yundonghui <dhsimpson@donnie4567.com>
2022-12-28 01:13:03 +0900

    feat | const highlightColor 추가 (settings 에선 알람 활성화 상태에 사용)

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 
 Color kakaoColor = ColorUtils.convertHexToColor(hexColorCode: "#FEE600");
 Color themeColor = ColorUtils.convertHexToColor(hexColorCode: "#1A1F24");
+Color highlightColor = ColorUtils.convertHexToColor(hexColorCode: "#0d2140");
 
 const double mainLogoSize = 60.0;
