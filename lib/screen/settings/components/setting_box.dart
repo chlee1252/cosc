78a144ba2a70855091dc1b00ad596e46638fa068
yundonghui <dhsimpson@donnie4567.com>
2022-12-28 21:49:40 +0900

    feat | 각 row 가 메서드를 넘겨받도록 변경

--- lib/screen/settings/components/setting_box.dart
+++ lib/screen/settings/components/setting_box.dart
@@ -3,24 +3,28 @@ import 'package:cosc/constants.dart';
 
 class SettingBox extends StatelessWidget {
   final Widget? child;
+  final VoidCallback? onTapCallback;
+
   static const double containerPaddingLeftRight = 20;
   static const double containerPaddingTopBottom = 0;
 
-  const SettingBox({Key? key, @required this.child}) : super(key: key);
+  const SettingBox({Key? key, required this.onTapCallback, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(containerPaddingLeftRight, containerPaddingTopBottom, containerPaddingLeftRight, containerPaddingTopBottom),
-      width: double.infinity,
-      height: 80,
-      color: themeColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [child!],
+    return InkWell(
+      onTap: onTapCallback,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(containerPaddingLeftRight, containerPaddingTopBottom, containerPaddingLeftRight, containerPaddingTopBottom),
+        width: double.infinity,
+        height: 80,
+        color: themeColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [child!],
+        ),
       ),
-      // alignment: Alignment.center,
     );
   }
 }
--- lib/screen/settings/setting_screen.dart
+++ lib/screen/settings/setting_screen.dart
@@ -7,19 +7,23 @@ import 'package:flutter/material.dart';
 class SettingScreen extends StatelessWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
+  // route 시키는 메서드 하나
+
+  // 모달 띄우는 메서드 하나
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            SettingBox(child: AlertSwitch()),
-            SettingBox(child: SettingText('관심 카테고리 설정')),
-            SettingBox(child: SettingText('이용약관')),
-            SettingBox(child: Version()),
-            SettingBox(child: SettingText('로그아웃')),
-            SettingBox(child: SettingText('계정탈퇴')),
-            SettingBox(child: SettingText('고객센터')),
+          children: [
+            SettingBox(onTapCallback: () {}, child: const AlertSwitch()),
+            SettingBox(onTapCallback: () {}, child: const SettingText('관심 카테고리 설정')),
+            SettingBox(onTapCallback: () {}, child: const SettingText('이용약관')),
+            SettingBox(onTapCallback: () {}, child: const Version()),
+            SettingBox(onTapCallback: () {}, child: const SettingText('로그아웃')),
+            SettingBox(onTapCallback: () {}, child: const SettingText('계정탈퇴')),
+            SettingBox(onTapCallback: () {}, child: const SettingText('고객센터')),
           ]
       ),
     );
