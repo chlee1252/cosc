87213178d587bd88e160dbd74c57f1519b236088
Marc <chlee1252@gmail.com>
2023-01-11 01:59:43 +0900

    fix: 색상 변경

--- lib/screen/settings/components/setting_box.dart
+++ lib/screen/settings/components/setting_box.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:cosc/constants.dart';
+import 'package:flutter/material.dart';
 
 class SettingBox extends StatelessWidget {
   final Widget? child;
@@ -8,18 +8,21 @@ class SettingBox extends StatelessWidget {
   static const double containerPaddingLeftRight = 20;
   static const double containerPaddingTopBottom = 0;
 
-  const SettingBox({Key? key, required this.onTapCallback, required this.child}) : super(key: key);
+  const SettingBox({Key? key, required this.onTapCallback, required this.child})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTapCallback,
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: containerPaddingTopBottom, horizontal: containerPaddingLeftRight),
+        padding: const EdgeInsets.symmetric(
+            vertical: containerPaddingTopBottom,
+            horizontal: containerPaddingLeftRight),
         // padding: const EdgeInsets.fromLTRB(containerPaddingLeftRight, containerPaddingTopBottom, containerPaddingLeftRight, containerPaddingTopBottom),
         width: double.infinity,
         height: 80,
-        color: themeColor,
+        color: firstThemeColor,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
