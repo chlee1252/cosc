dcbeec00829916b987306de54848cbd93ae8b84d
yundonghui <qeadzc9503@naver.com>
2023-02-12 14:51:04 +0900

    refactor | indenting

--- lib/screen/category/components/Language.dart
+++ lib/screen/category/components/Language.dart
@@ -12,25 +12,27 @@ class Language extends StatelessWidget {
   Widget build(BuildContext context) {
     return
       Container(
-      padding: const EdgeInsets.only(top: 20),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: isSelected! ? secondThemeColor : firstThemeColor,
-      ),
-      child: Column(children: [
-        Image.asset(LanguageType.getByCode(code).asset, width: 110, height: 110,),
-        Container(
-          alignment: Alignment.center,
-          margin: const EdgeInsets.symmetric(vertical: 15),
-          padding: const EdgeInsets.symmetric(vertical: 5),
-          decoration: BoxDecoration(
+        padding: const EdgeInsets.only(top: 20),
+        decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10.0),
-            color:  firstPointColor,
-          ),
-          width: 100,
-          child: Text(LanguageType.getByCode(code).name, style: const TextStyle(color: Colors.white, fontSize: 16)),
+            color: isSelected! ? secondThemeColor : firstThemeColor,
         ),
-      ],),
+        child: Column(
+          children: [
+            Image.asset(LanguageType.getByCode(code).asset, width: 110, height: 110,),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color:  firstPointColor,
+              ),
+              width: 100,
+              child: Text(LanguageType.getByCode(code).name, style: const TextStyle(color: Colors.white, fontSize: 16)),
+            ),
+          ],
+        ),
     );
   }
 }
