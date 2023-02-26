7156a442a4f34d018ab8f694928a3c055d6caa57
yundonghui <qeadzc9503@naver.com>
2023-02-26 16:20:49 +0900

    feat | 문제 카드 스와이핑 적용

--- lib/screen/quiz/quiz_sceen.dart
+++ lib/screen/quiz/quiz_sceen.dart
@@ -1,18 +1,38 @@
 import 'package:cosc/screen/quiz/components/question.dart';
 import 'package:cosc/widget/cosc_appbar.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class QuizScreen extends StatelessWidget {
   const QuizScreen({Key? key}) : super(key: key);
-
+  static const QuestionList = [
+    Question(),
+    Question(),
+    Question(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         appBar: CoscAppBar(),
-        body: Center(
-        child: Question(),
-      )
+        body: Swiper(
+            itemBuilder: (BuildContext context,int index){
+              return QuestionList[index];
+            },
+            itemCount: 3,
+            layout: SwiperLayout.TINDER,
+            itemWidth: double.infinity,
+            itemHeight: double.infinity,
+            loop: false,
+            curve: Curves.easeInOutBack
+          )
     );
   }
 
+  // child: Swiper(
+  // itemBuilder: (BuildContext context,int index){
+  // return Question();
+  // },
+  // itemCount: 1,
+  // ),
 }
