0f51bd7e5c58546ae2ae5e04b82c48aef8d05852
Changhwan Lee <marc@Changhwans-MacBook-Pro-2.local>
2023-05-17 20:27:13 +0900

    feat: deeplink redirection 세팅 및 토큰 값 저장

--- lib/service/auth/auth_controller.dart
+++ lib/service/auth/auth_controller.dart
@@ -1,5 +1,44 @@
+import 'package:cosc/commons/data/data.dart';
 import 'package:get/get.dart';
+import 'package:uni_links/uni_links.dart';
 
 class AuthController extends GetxController {
   bool isLogin = true;
+  var token = RxString("");
+
+  setToken(String newToken) {
+    token.value = newToken;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    linkStream.listen((String? link) {
+      handleLink(link!);
+    }, onError: (error) {
+      print("Error!");
+    });
+  }
+
+  void handleLink(String link) async {
+    Uri uri;
+    try {
+      uri = Uri.parse(link);
+    } catch (e) {
+      throw Exception("Login 실패!");
+    }
+
+    if (uri.scheme == 'cosc') {
+      final accessToken = uri.queryParameters[ACCESS_TOKEN_KEY];
+      final refreshToken = uri.queryParameters[REFRESH_TOKEN_KEY];
+
+      if (accessToken == null || refreshToken == null) {
+        throw Exception("Login 실패"); // TODO: 로그인 실패
+      }
+
+      await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+      await storage.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
+    }
+  }
 }
