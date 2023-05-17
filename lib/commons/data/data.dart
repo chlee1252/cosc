ed525838b58061f76809ea5aca8939f742f02aa3
Changhwan Lee <marc@Changhwans-MacBook-Pro-2.local>
2023-05-17 20:26:30 +0900

    feat: flutter_secure_storage 추가

---
+++ lib/commons/data/data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+const ACCESS_TOKEN_KEY = "accessToken";
+const REFRESH_TOKEN_KEY = "refreshToken";
+
+const storage = FlutterSecureStorage();
