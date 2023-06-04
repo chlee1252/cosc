49ba54d0a9b933b0a187627a6ce0069be66035e5
Marc Lee <chlee1252@gmail.com>
2023-06-04 20:59:06 +0900

    feat: 프로젝트 타이틀 변경

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Cosc',
       theme: ThemeData(
         scaffoldBackgroundColor: firstThemeColor,
       ),
